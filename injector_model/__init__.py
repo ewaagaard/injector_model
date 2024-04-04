a40345e416d85f175519f81a8762dd0257f558fa
elias <elias.waagaard@gmail.com>
2024-04-04 18:12:13 +0200

    updated imports

--- injector_model/__init__.py
+++ injector_model/__init__.py
@@ -1,6 +1,6 @@
 from .injector_model import InjectorChain
-#from .injector_model_full_sc_integral import InjectorChain_full_SC
-from .injection_energies import InjectionEnergies
+from .injector_model_full_sc_integral import InjectorChain_full_SC
 from .space_charge_and_ibs import IBS_Growth_Rates, SC_Tune_Shifts
-from .parameters_and_helpers import Inj_Parameters, BeamParams_LEIR, BeamParams_PS, BeamParams_SPS
-from .sequence_makers import Sequences, 
+from .parameters_and_helpers import Reference_Values, BeamParams_LEIR, BeamParams_PS, BeamParams_SPS
+from .sequence_makers import Sequences
+from .injection_energies import InjectionEnergies
