46b767270c88fb8d1ccf8ee8d76168f10a550308
elias <elias.waagaard@gmail.com>
2024-04-05 17:29:36 +0200

    updated tests

--- tests/test_injector_model_simple_calculation.py
+++ tests/test_injector_model_simple_calculation.py
@@ -4,5 +4,5 @@ Test calculating propagated beam intensity with the full space charge integral
 from injector_model import InjectorChain_v2
 
 # Instantiate injector chain version 2 and calculate LHC bunch intensity for all ions
-injector_chain = InjectorChain_v2()
+injector_chain = InjectorChain_v2(nPulsesLEIR=7)  # 7 OR DEFAULT NUMBER?
 df = injector_chain.calculate_LHC_bunch_intensity_all_ion_species()
