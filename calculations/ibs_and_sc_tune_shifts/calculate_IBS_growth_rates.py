4508399f76059bfea0279e27419abeb301fd8676
elwaagaa <elias.walter.waagaard@cern.ch>
2024-09-03 14:44:32 +0200

    IBS calculations

--- calculations/ibs_and_sc_tune_shifts/calculate_IBS_growth_rates.py
+++ calculations/ibs_and_sc_tune_shifts/calculate_IBS_growth_rates.py
@@ -9,4 +9,5 @@ df_IBS = inj.calculate_IBS_growth_rates_all_ion_species()
 
 # Calculate relative growth rates - divide by Pb column
 df_IBS_rel = df_IBS.div(df_IBS['Pb'], axis=0)
+df_IBS_rel = df_IBS_rel.round(2) # remove too many digits
 df_IBS_rel.to_csv('output_csv/IBS_growth_rates_relative.csv')
