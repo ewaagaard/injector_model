3640755b3e7b103f528081a50c94563256bf0101
elias <elias.waagaard@gmail.com>
2024-09-29 20:02:37 +0200

    added calculation values

--- calculations/scenarious_v1_with_ecooling/combined_charge_and_isotope_scan_results/generate_optimistic_and_conservative_scenario.py
+++ calculations/scenarious_v1_with_ecooling/combined_charge_and_isotope_scan_results/generate_optimistic_and_conservative_scenario.py
@@ -10,17 +10,22 @@ from injector_model import InjectorChain
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import defaultdict
+import os
+
+os.makedirs('output', exist_ok=True)
 
 # Consider PS space charge limit to be at present Pb tune shifts, or not
 consider_PS_SC_limit = False
 
 if consider_PS_SC_limit:
     load_string = 'scenarios_with_ecooling'
-    output_extra_str = '_with_PS_SC_limit'
+    PS_string = 'with_PS_limit'
 else:
     load_string = 'test_no_PS_space_charge_limit'
-    output_extra_str = '_no_PS_SC_limit'
-    
+    PS_string = 'no_PS_limit'
+
+# Define output string for optimistic value
+ions_not_stripped = ['He', 'O', 'Mg', 'Ar', 'Kr']
 
 # Load reference ion data
 ion_data = pd.read_csv("../../../data/Ion_species.csv", header=0, index_col=0).T
@@ -28,11 +33,11 @@ mass_number = ion_data.loc['A']
 ref_Table_SPS = pd.read_csv('../../../data/test_and_benchmark_data/SPS_final_intensities_WG5_and_Hannes.csv', index_col=0)
 
 # Load charge state scan - no PS space charge limit
-with open("../charge_state_scan_no_PS_limit/output/charge_state_scan.json", "r") as fp:
+with open("../charge_state_scan_{}/output/charge_state_scan.json".format(PS_string), "r") as fp:
    charge_dict = json.load(fp)
 
 # Load isotope scan
-with open("../isotope_scan_no_PS_limit/output/isotope_scan.json", "r") as fp:
+with open("../isotope_scan_{}/output/isotope_scan.json".format(PS_string), "r") as fp:
    isotope_dict = json.load(fp)
 
 # Convert strings to numpy arrays
@@ -43,40 +48,48 @@ isotope_dict['Best A'] = np.array(list(map(float, isotope_dict['Best A'])))
 
 #### CONSERVATIVE ####
 # Load conservative result - from baseline scenario
-full_result_conservative = pd.read_csv(...)
+full_result_conservative = pd.read_csv('../scenarios_with_ecooling/output_csv/1_baseline.csv', index_col=0)
 
 #### OPTIMISTIC ####
-# --> find currents, charge states (isotopes not for )
-
-# Get current for best Q and A from Json
-# Update number of allowed LEIR injections?
-
+# --> find currents, charge states (isotopes not for now)
+# Use best charge state, its current and generously round up to nearest number of LEIR injections
 
 # Initialize full dicionary
 full_result_optimistic = defaultdict(list)
 
 # Run cases with best Q, best A and updated LINAC current
-for ion_type in ion_data.columns:
+for i, ion_type in enumerate(ion_data.columns):
 
-        custom_ion_data = injector_chain2.full_ion_data[ion_type].copy() # copy entry
-        custom_ion_data['Q before stripping'] = Q
-        custom_ion_data['Linac3 current [uA]'] = current
+        # Load current from charge state scan
+        PS_case  = 'no_PS_SC_limit' if not consider_PS_SC_limit else 'with_PS_SC_limit'
+        output_extra_str='UNSTRIPPED_{}'.format(PS_case) if ion_type in ions_not_stripped else 'STRIPPED_{}'.format(PS_case)
+        read_str = '../charge_state_scan_{}/output/charge_scan_results/{}_2_no_PS_splitting_{}_with_ecooling_limits.csv'.format(PS_string, ion_type, output_extra_str)       
+        df_charge_state_scan = pd.read_csv(read_str, index_col=0)
+
+        # Build custom input data
+        custom_ion_data = ion_data[ion_type].copy() # copy entry
+        Q = charge_dict['Best Q'][i]
+        current = df_charge_state_scan['{}'.format(int(Q))].loc['Linac3_current [A]']
+        custom_ion_data['Q before stripping'] = Q   
+        custom_ion_data['Linac3 current [uA]'] = float(current) * 1e6 # convert from A to uA
         print('Q strip = {} has Linac3 current: {} uA'.format(Q, current))
 
         # Optimistic case - recalculate
-        injector_chain2 = InjectorChain(PS_splitting = 1,
-                                        account_for_LEIR_ecooling=True,
-                                        PS_factor_SC = np.inf
-                                    )
-        
-        result_optimistic = injector_chain2.calculate_LHC_bunch_intensity()
+        inj_optimistic = InjectorChain(PS_splitting = 1,
+                                       account_for_LEIR_ecooling=True,
+                                       PS_factor_SC = np.inf,
+                                       round_number_of_LEIR_inj_up=True
+                                       )
+        inj_optimistic.init_ion(ion_type=ion_type, ion_data_custom=custom_ion_data)        
+        result_optimistic = inj_optimistic.calculate_LHC_bunch_intensity()
 
         # Append the values to the corresponding key 
         for key, value in result_optimistic.items():
             full_result_optimistic[key].append(value)
 
-        injector_chain2.init_ion(ion_type=ion_type, ion_data_custom=custom_ion_data)
-        df2 = injector_chain2.calculate_LHC_bunch_intensity()
-
-# Make dataframe of optimistic scenario
+# Make dataframe of optimistic scenario - save as csv and excel
 df_optimistic = pd.DataFrame(full_result_optimistic)
+df_optimistic.T.to_csv('output/2_no_PS_bunch_splitting_optimistic.csv')
+#df_optimistic.T.to_excel('output/2_no_PS_bunch_splitting_optimistic.xlsx')
+full_result_conservative.to_csv('output/2_no_PS_bunch_splitting_conservative.csv')
+#full_result_conservative.to_excel('output/2_no_PS_bunch_splitting_conservative.xlsx')
