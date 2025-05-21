b6ca289908b8c316fb6bcb0493363215bc0ff7b9
elwaagaa <elias.walter.waagaard@cern.ch>
2025-05-21 17:14:26 +0200

    O tests for linac currents

---
+++ calculations/results_2025_parameters/000_1st_test_baseline_scenarios/test_linac3_current_for_O.py
@@ -0,0 +1,14 @@
+"""
+Small test script for varying LINAC3 current for oxygen
+"""
+import matplotlib.pyplot as plt
+import pandas as pd 
+from injector_model import InjectorChain
+
+inj1 = InjectorChain(ion_type='O')
+result_70A = inj1.calculate_LHC_bunch_intensity()
+
+inj2 = InjectorChain(ion_type='O')
+inj2.linac3_current = 90e-6
+result_90A = inj2.calculate_LHC_bunch_intensity()
+
